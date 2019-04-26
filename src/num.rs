2869febf5585fba299de693dea81d89461c12672
Daniel Mueller <deso@posteo.net>
2019-04-26 16:37:05 -0700

    Add support for deserialization from ints and floats
    We initially started off working under the assumption that a Num will be
    created from a string, given that the first crate we used this crate in
    represented serialized numbers as strings. However, that will not always
    be the case.
    To be more flexible and future proof, this change also adds support for
    creating a Num by deserializing an integer or floating point value.

--- src/num.rs
+++ src/num.rs
@@ -10,6 +10,7 @@ use std::str::FromStr;
 
 use serde::de::Error;
 use serde::de::Unexpected;
+use serde::de::Visitor;
 use serde::Deserialize;
 use serde::Deserializer;
 use serde::Serialize;
@@ -134,11 +135,55 @@ impl<'de> Deserialize<'de> for Num {
   where
     D: Deserializer<'de>,
   {
-    let s = String::deserialize(deserializer)?;
-    let v = Num::from_str(&s)
-      .map_err(|_| Error::invalid_value(Unexpected::Str(&s), &"unable to parse string as Num"))?;
+    struct NumVisitor;
 
-    Ok(v)
+    impl<'de> Visitor<'de> for NumVisitor {
+      type Value = Num;
+
+      fn expecting(&self, formatter: &mut Formatter<'_>) -> FmtResult {
+        formatter.write_str("a float, an integer, or a string representing either")
+      }
+
+      fn visit_str<E>(self, s: &str) -> Result<Self::Value, E>
+      where
+        E: Error,
+      {
+        Num::from_str(s)
+          .map_err(|_| Error::invalid_value(Unexpected::Str(&s), &"a stringified integer/float"))
+      }
+
+      fn visit_i64<E>(self, v: i64) -> Result<Self::Value, E>
+      where
+        E: Error,
+      {
+        let s = format!("{}.0", v.to_string());
+        Num::from_str(&s)
+          .map_err(|_| Error::invalid_value(Unexpected::Str(&s), &"a signed integer"))
+      }
+
+      fn visit_u64<E>(self, v: u64) -> Result<Self::Value, E>
+      where
+        E: Error,
+      {
+        let s = format!("{}.0", v.to_string());
+        Num::from_str(&s)
+          .map_err(|_| Error::invalid_value(Unexpected::Str(&s), &"an unsigned integer"))
+      }
+
+      fn visit_f64<E>(self, v: f64) -> Result<Self::Value, E>
+      where
+        E: Error,
+      {
+        // TODO: This is fairly clumsy and introduces an unnecessary
+        //       error path. What we really want is a straight
+        //       conversion without an intermediate string.
+        let s = v.to_string();
+        Num::from_str(&s)
+          .map_err(|_| Error::invalid_value(Unexpected::Str(&s), &"a floating point value"))
+      }
+    }
+
+    deserializer.deserialize_any(NumVisitor)
   }
 }
 
@@ -278,11 +323,27 @@ mod tests {
   }
 
   #[test]
-  fn deserialize_json() {
+  fn deserialize_json_string() {
     let json = r#""37.519""#;
     let num = from_json::<Num>(&json).unwrap();
 
     assert_eq!(num, Num::new(37519, 1000));
+  }
+
+  #[test]
+  fn deserialize_json_float() {
+    let json = r#"126.2633"#;
+    let num = from_json::<Num>(&json).unwrap();
+
+    assert_eq!(num, Num::new(1262633, 10000));
+  }
+
+  #[test]
+  fn deserialize_json_int() {
+    let json = r#"356"#;
+    let num = from_json::<Num>(&json).unwrap();
+
+    assert_eq!(num, Num::from_int(356));
   }
 
   #[test]
