d3700cb7e9139adc37f01c86ef471aa58d1fa492
Daniel Mueller <deso@posteo.net>
2020-04-02 07:26:52 -0700

    Enable a couple of standard warnings
    This change enables a couple of more or less standard warnings that will
    hopefully allow us to improve code quality over time.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,48 @@
-// Copyright (C) 2019 Daniel Mueller <deso@posteo.net>
+// Copyright (C) 2019-2020 Daniel Mueller <deso@posteo.net>
 // SPDX-License-Identifier: GPL-3.0-or-later
+
+#![warn(
+  bad_style,
+  dead_code,
+  future_incompatible,
+  illegal_floating_point_literal_pattern,
+  improper_ctypes,
+  intra_doc_link_resolution_failure,
+  late_bound_lifetime_arguments,
+  missing_copy_implementations,
+  missing_debug_implementations,
+  missing_docs,
+  no_mangle_generic_items,
+  non_shorthand_field_patterns,
+  nonstandard_style,
+  overflowing_literals,
+  path_statements,
+  patterns_in_fns_without_body,
+  private_in_public,
+  proc_macro_derive_resolution_fallback,
+  renamed_and_removed_lints,
+  rust_2018_compatibility,
+  rust_2018_idioms,
+  safe_packed_borrows,
+  stable_features,
+  trivial_bounds,
+  trivial_numeric_casts,
+  type_alias_bounds,
+  tyvar_behind_raw_pointer,
+  unconditional_recursion,
+  unreachable_code,
+  unreachable_patterns,
+  unstable_features,
+  unstable_name_collisions,
+  unused,
+  unused_comparisons,
+  unused_import_braces,
+  unused_lifetimes,
+  unused_qualifications,
+  unused_results,
+  where_clauses_object_safety,
+  while_true,
+)]
 
 //! A crate containing a number type suitable for use in financial
 //! contexts.
